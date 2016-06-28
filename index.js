5a77567aa841c8294730d7348c7e2e18b0ef7341
Dominik Kundel <dominik.kundel@gmail.com>
2016-06-28 16:59:39 +0200

    receiving SMS!

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const bodyParser = require('body-parser');
 const chalk = require('chalk');
 const twilio = require('twilio');
+const restify = require('restify');
 
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 const success = chalk.bold.green;
 const error = chalk.bold.red;
+const PORT = process.env.PORT || 3000;
 
 const { Twitch } = require('./src/twitch');
 
@@ -34,6 +37,24 @@ Twitch.on('went-offline', ({ name }) => {
   notifyUsers(name, `Hi! ${name} just went offline :(!`);
 });
 
-Twitch.subscribe('dkundel', process.env.MY_PHONE_NUMBER);
-Twitch.subscribe('marcos_placona', process.env.MY_PHONE_NUMBER);
-Twitch.startPoll(5);
+// Twitch.subscribe('dkundel', process.env.MY_PHONE_NUMBER);
+// Twitch.subscribe('marcos_placona', process.env.MY_PHONE_NUMBER);
+// Twitch.startPoll(5);
+
+const server = restify.createServer();
+
+// parse application/x-www-form-urlencoded 
+server.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+server.use(bodyParser.json())
+
+server.post('/sms', (req, res, next) => {
+  console.log(req.body.Body);
+  res.header('Content-Type', 'text/plain');
+  res.send('Thanks for subscribing!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
