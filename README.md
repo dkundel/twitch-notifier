bc8c2c8ca62c1241c95b38ca5296af783fc4fa09
Dominik Kundel <dominik.kundel@gmail.com>
2016-06-28 16:42:52 +0200

    Sending SMS! :)

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ An SMS notifier for Twitch streams
 
 1. Get a client ID and set it as an environment variable `TWITCH_CLIENT_ID`
 
-2. Execute:
+2. Register with Twilio and store your credentials as `TWILIO_ACCOUNT_SID` and `TWILIO_AUTH_TOKEN`
+
+3. Store your own phone number as `MY_PHONE_NUMBER`
+
+4. Execute:
 
 ```bash
 git clone https://github.com/dkundel/twitch-notifier.git
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const chalk = require('chalk');
+const twilio = require('twilio');
+
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 const success = chalk.bold.green;
 const error = chalk.bold.red;
@@ -7,14 +10,30 @@ const { Twitch } = require('./src/twitch');
 
 console.log('Ahoy!!');
 
+function notifyUsers(name, message) {
+  if (Twitch.subscribers.has(name)) {
+    Twitch.subscribers.get(name).forEach((number) => {
+      client.sendMessage({
+        from: '+4915735982633',
+        to: number,
+        body: message
+      }).then(() => {
+        console.log(success('MESSAGE SENT!'));
+      })
+    });
+  }
+}
+
 Twitch.on('went-online', ({ name }) => {
   console.log(`${name} went ${success('online')}!`);
+  notifyUsers(name, `Hi! ${name} just went online on Twitch!`);
 });
 
 Twitch.on('went-offline', ({ name }) => {
   console.log(`${name} went ${error('offline')}!`);
+  notifyUsers(name, `Hi! ${name} just went offline :(!`);
 });
 
-Twitch.subscribe('dkundel', '+11111111111');
-Twitch.subscribe('marcos_placona', '+1111111111');
+Twitch.subscribe('dkundel', process.env.MY_PHONE_NUMBER);
+Twitch.subscribe('marcos_placona', process.env.MY_PHONE_NUMBER);
 Twitch.startPoll(5);
