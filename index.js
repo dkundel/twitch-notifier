7cb417f8f7501487bd39abfc116185a6fbfd33c1
Dominik Kundel <dominik.kundel@gmail.com>
2016-06-28 16:16:07 +0200

    Subscribing, proper maps :)

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const chalk = require('chalk');
+
+const success = chalk.bold.green;
+const error = chalk.bold.red;
+
 const { Twitch } = require('./src/twitch');
 
 console.log('Ahoy!!');
 
 Twitch.on('went-online', ({ name }) => {
-  console.log(`${name} went online!`);
-})
+  console.log(`${name} went ${success('online')}!`);
+});
 
+Twitch.on('went-offline', ({ name }) => {
+  console.log(`${name} went ${error('offline')}!`);
+});
+
+Twitch.subscribe('dkundel', '+11111111111');
+Twitch.subscribe('marcos_placona', '+1111111111');
 Twitch.getStreams();
--- src/twitch.js
+++ src/twitch.js
@@ -6,10 +6,6 @@ const BASE_URL = 'https://api.twitch.tv/kraken/';
 const CONTENT_TYPE = 'application/vnd.twitchtv.v3+json';
 
 class Twitch extends EventEmitter {
-  constructor() {
-    this.currentlyOnline = new Map();
-    this.subscribers = new Map();
-  }
 
   makeRequest(endPoint, params, callback) {
     let options = {
@@ -31,16 +27,60 @@ class Twitch extends EventEmitter {
     });
   }
 
+  init() {
+    this.subscribers = this.subscribers || new Map();
+    this.currentlyOnline = this.currentlyOnline || new Map();
+  }
+
+  setStreamOnline(name) {
+    this.currentlyOnline.set(name, true);
+    this.emit('went-online', { name });
+  }
+
+  setStreamOffline(name) {
+    this.currentlyOnline.delete(name);
+    this.emit('went-offline', { name });
+  }
+
+  iteratorToArray(iterator) {
+    let array = [];
+    for (let i of iterator) {
+      array.push(i);
+    }
+
+    return array;
+  }
+
   getStreams() {
-    let channels = ['dkundel', 'marcos_placona'];
+    this.init();
+    let channels = this.iteratorToArray(this.subscribers.keys());
+
+    console.log(channels);
+
+    let notOnline = new Map();
+    for (let [channel, numbers] of this.subscribers) {
+      notOnline.set(channel, true);
+    }
 
     this.makeRequest('streams', { channel: channels.join(',') }, (err, resp, body) => {
       let name = body.streams[0].channel.name;
-      this.emit('went-online', { name });
+
+      body.streams.forEach((stream) => {
+        let name = stream.channel.name;
+        notOnline.delete(name);
+        if (!this.currentlyOnline.has(name)) {
+          this.setStreamOnline(name);
+        }
+      });
+
+      for (let [name, isOffline] of notOnline) {
+        this.setStreamOffline(name);
+      }
     });
   }
 
   subscribe(name, phoneNumber) {
+    this.init();
     if (this.subscribers.has(name)) {
       this.subscribers.get(name).push(phoneNumber);
     } else {
